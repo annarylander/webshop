541c1c818606adf3f8a09e32730b2c3b092141ee
Anna Rylander <annaelida.rylander@gmail.com>
2022-10-04 15:10:16 +0200

    routes in backend

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,11 +1,17 @@
 import express, { Application, json, Request, Response } from "express";
 import cors from "cors";
+import product from "./routes/product";
+import user from "./routes/user";
 
 const app: Application = express();
 app.use(cors());
 app.use(json());
 
 const port: number = parseInt(process.env.SERVER_PORT || "3001");
+
+app.use("/user", user);
+app.use("/product", product);
+
 app.get("/hello", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
---
+++ packages/server/src/routes/product.ts
@@ -0,0 +1,5 @@
+import express, { Request, Response } from "express";
+
+const productRouter = express.Router();
+
+export default productRouter;
