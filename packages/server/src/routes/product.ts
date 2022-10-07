f792454c3758e39b8efba1ff483ce8482013ea3c
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-10-07 11:31:20 +0200

    creating routes function for products post and get

--- packages/server/src/routes/product.ts
+++ packages/server/src/routes/product.ts
@@ -2,4 +2,12 @@ import express, { Request, Response } from "express";
 
 const productRouter = express.Router();
 
+productRouter.get("/", (req: Request, res: Response) => {
+  res.send("Getting some products");
+});
+
+productRouter.post("/", (req: Request, res: Response) => {
+  res.send("Posting some products");
+});
+
 export default productRouter;
