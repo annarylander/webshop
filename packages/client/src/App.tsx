8ab6a211ae9c9b510a90d7d34221341357e8a461
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-10-07 13:22:31 +0200

    Added Register & Login modal. No db connection yet

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import StartPage from "./pages/StartPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import AccountPage from "./pages/AccountPage";
 import CheckOutPage from "./pages/CheckOutPage";
@@ -13,8 +11,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/details/:slug" element={<ProductDetailPage />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/checkout" element={<CheckOutPage />} />
