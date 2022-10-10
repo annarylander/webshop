d435ed47a8a9668fa20392af9320e5413220dd28
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-10-10 15:56:55 +0200

    can now render products

--- packages/client/src/components/Card.tsx
+++ packages/client/src/components/Card.tsx
@@ -1,18 +1,19 @@
 import { Button } from "@chakra-ui/react";
+import { ProductItem } from "@my-webshop/shared";
 
-export default function Card() {
+export default function Card({_id, title, price, mainImage}: ProductItem) {
+
+  const ifNoImg = "https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBsYW50c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=300&q=20"
+
   return (
-    <div className="product-card">
+    <div className="product-card" key={_id}>
       <div>
-        <h3>Fiolfikus</h3>
-        <p>50 kr</p>
+        <h3>{title}</h3>
+        <p>{price} kr</p>
       </div>
 
       <div>
-        <img
-          src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHBsYW50c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=300&q=20"
-          alt="fikus"
-        />
+        <img src={mainImage === undefined ? ifNoImg : mainImage.url} alt={mainImage === undefined ? 'Fikus' : mainImage.alt} />
       </div>
 
       <div className="buy-button">
