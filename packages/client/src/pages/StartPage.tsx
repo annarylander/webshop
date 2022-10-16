02fe2bb4b2c7dd456dc9106844458dd15b6118ef
Anna Rylander <annaelida.rylander@gmail.com>
2022-10-16 17:08:46 +0200

    search function on startpage

--- packages/client/src/pages/StartPage.tsx
+++ packages/client/src/pages/StartPage.tsx
@@ -2,13 +2,17 @@ import React, { useEffect } from "react";
 import Card from "../components/Card";
 import axios from "axios";
 import { ProductItem } from "@my-webshop/shared";
+import { Input } from "@chakra-ui/react";
 
 export default function StartPage() {
   const [products, setProducts] = React.useState<ProductItem[]>([]);
   const [error, setError] = React.useState<string | undefined>();
+  const [search, setSearch]: [string, (search: string) => void] =
+    React.useState("");
 
   const productsURL: string =
-    process.env.REACT_APP_PRODUCTS_URL || "http://localhost:3002/product";
+    `${process.env.REACT_APP_PRODUCTS_URL}/product` ||
+    "http://localhost:3002/product";
 
   useEffect(() => {
     axios
@@ -22,14 +26,61 @@ export default function StartPage() {
       });
   }, []);
 
-  const ProductList = ({ products, error }: { products: ProductItem[]; error?: string }) => {
+  // const ProductList = ({
+  //   products,
+  //   error,
+  // }: {
+  //   products: ProductItem[];
+  //   error?: string;
+  // }) => {
+  //   if (error) {
+  //     return <div>{error}</div>;
+  //   } else if (products) {
+  //     return (
+  //       <div className="products">
+  //         {products.map((product) => {
+  //           return (
+  //             <Card
+  //               _id={product._id}
+  //               title={product.title}
+  //               price={product.price}
+  //               mainImage={product.mainImage}
+  //             />
+  //           );
+  //         })}
+  //       </div>
+  //     );
+  //   } else {
+  //     return <div>'Waiting for products'</div>;
+  //   }
+  // };
+
+  const ProductList = ({
+    products,
+    error,
+  }: {
+    products: ProductItem[];
+    error?: string;
+  }) => {
     if (error) {
       return <div>{error}</div>;
     } else if (products) {
       return (
         <div className="products">
           {products.map((product) => {
-            return <Card _id={product._id} title={product.title} price={product.price} mainImage={product.mainImage}/>;
+            if (
+              search === "" ||
+              product.title.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return (
+                <Card
+                  _id={product._id}
+                  title={product.title}
+                  price={product.price}
+                  mainImage={product.mainImage}
+                />
+              );
+            }
           })}
         </div>
       );
@@ -45,9 +96,15 @@ export default function StartPage() {
           <h2>Green vibes only</h2>
         </div>
       </div>
-   
-        <ProductList products={products} error={error} />
-    
+      <div className="search">
+        <p>Search product 🔍</p>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        ></input>
+      </div>
+      <ProductList products={products} error={error} />
     </div>
   );
 }
