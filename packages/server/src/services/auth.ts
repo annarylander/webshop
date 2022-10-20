1a00ca55686d206e425fd18c4d7bb0c4d044713f
IzaMarkstrom <74114062+IzaMarkstrom@users.noreply.github.com>
2022-10-20 10:25:29 +0200

    Added account page and toggle signup button

--- packages/server/src/services/auth.ts
+++ packages/server/src/services/auth.ts
@@ -12,15 +12,12 @@ export const authUser = (req: Request, res: Response, next: any) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  console.log(req.body.full_name, "req body");
-
   if (token == null) return res.status(401).send("No token exist.");
 
   jwt.verify(
     token,
     process.env.TOKEN_SECRET as string,
     (err: any, user: any) => {
-      console.log(err, "error verify");
 
       if (err) return res.status(403).send("Token not verifiable");
 
