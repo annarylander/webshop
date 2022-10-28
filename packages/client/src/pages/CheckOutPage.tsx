5f22da59c26fbcc8408a2f740ef441356fd9df2b
Anna Rylander <annaelida.rylander@gmail.com>
2022-10-28 11:41:31 +0200

    checkout page w basic styling and icons

--- packages/client/src/pages/CheckOutPage.tsx
+++ packages/client/src/pages/CheckOutPage.tsx
@@ -1,14 +1,29 @@
 import React, { useContext } from "react";
+import ListCartItems from "../components/ListCartItems";
 import UserContext from "../context/UserContext";
+import { Button, Box, Icon } from "@chakra-ui/react";
+import { FaCcMastercard, FaCcPaypal, FaCcVisa } from "react-icons/fa";
 
 export default function CheckOutPage() {
   const { user } = React.useContext(UserContext);
-  console.log("checked out", user);
+  console.log("current", user);
   return (
-    <div>
-      <h1>Checkout page</h1>
-      <div>is logged in: {user?.email}</div>
-      <p>{user?.full_name}</p>
+    <div className="checkout-page">
+      <Box fontWeight="semibold">
+        <h1>Shoppingcart </h1>
+      </Box>
+
+      <Box mt={4}>
+        <ListCartItems />
+      </Box>
+
+      <Button colorScheme="green" mt={4}>
+        Proceed to checkout
+      </Button>
+      <Box mt={4} fontSize="md" color="gray.600">
+        We accept: <Icon as={FaCcPaypal} /> <Icon as={FaCcVisa} />{" "}
+        <Icon as={FaCcMastercard} />
+      </Box>
     </div>
   );
 }
