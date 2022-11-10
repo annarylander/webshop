4033522679b24abf3ecaa58810d9273c7d34adf1
Mika Hägerström <mikahagerstrom@hotmail.com>
2022-11-10 10:44:25 +0100

    formatted productdetailmodal

--- packages/client/src/components/ProductDetailModal.tsx
+++ packages/client/src/components/ProductDetailModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Modal,
   ModalOverlay,
@@ -8,35 +8,37 @@ import {
   ModalCloseButton,
   useDisclosure,
   Button,
-  Link
-} from '@chakra-ui/react'
-import DetailView from './DetailView'
-import { ProductItem } from '@my-webshop/shared';
-export default function ProductDetailModal(props: {product: ProductItem, error?: string}) {
-
-    const { isOpen, onOpen, onClose } = useDisclosure()
+  Link,
+} from "@chakra-ui/react";
+import DetailView from "./DetailView";
+import { ProductItem } from "@my-webshop/shared";
+export default function ProductDetailModal(props: {
+  product: ProductItem;
+  error?: string;
+}) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  
   return (
-    <div> <Link onClick={onOpen}>{props.product.title}</Link>
+    <div>
+      {" "}
+      <Link onClick={onOpen}>{props.product.title}</Link>
+      <Modal isOpen={isOpen} onClose={onClose} size="full">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <div>
+              <DetailView product={props.product} error={props.error} />
+            </div>
+          </ModalBody>
 
-    <Modal isOpen={isOpen} onClose={onClose} size='full'>
-      <ModalOverlay />
-      <ModalContent>
-       
-        <ModalCloseButton />
-        <ModalBody>
-       
-        <div>
-          <DetailView product={props.product} error={props.error} />
-        </div>
-   
-        </ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme='green' mr={3} onClick={onClose}>
-            Close
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal></div>
-  )
+          <ModalFooter>
+            <Button colorScheme="green" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
 }
